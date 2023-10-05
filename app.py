a4212dd183cb29bfc4d38f89836ec382b95e1958
Sarmita Majumdar <sarmita.majumdar@gmail.com>
2023-10-05 20:14:06 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,61 @@
+from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+import numpy as np
+from io import BytesIO
+from PIL import Image
+import tensorflow as tf
+
+import nest_asyncio
+
+nest_asyncio.apply()
+
+app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+MODEL = tf.keras.models.load_model(
+    r"/Users/Sarmita Majumdar/Desktop/SMit_App/WebApp-Diabetic-Retino/models_epoch/model00000002-0.9092040061950684.h5")
+
+CLASS_NAMES = ["DR", "No_DR"]
+
+
+@app.get("/ping")
+async def ping():
+    return "Hi, I am still here! SM"
+
+
+def read_file_as_image(data) -> np.ndarray:
+    image = np.array(Image.open(BytesIO(data)))
+    return image
+
+
+@app.post("/predict")
+async def predict(
+        file: UploadFile = File(...)
+):
+    image = read_file_as_image(await file.read())
+    img_batch = np.expand_dims(image, 0)
+
+    predictions = MODEL.predict(img_batch)
+
+    predicted_class = CLASS_NAMES[np.argmax(predictions[0])]
+    confidence = np.max(predictions[0])
+    return {
+        'class': predicted_class,
+        'confidence': float(confidence)
+    }
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host='localhost', port=8888)
